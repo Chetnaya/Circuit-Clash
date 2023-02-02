e3a3eefa862cac5c70e38244cee55092b6cdcc11
Chetnaya Kalyan <90476376+Chetnaya@users.noreply.github.com>
2023-02-02 10:53:21 +0530

    Quit Button Functionality

--- Cars Race/Assets/Scripts/MainMenu.cs
+++ Cars Race/Assets/Scripts/MainMenu.cs
@@ -10,4 +10,17 @@ public class MainMenu : MonoBehaviour
         //Loads the Scene 01 on clicking "Pkay in the menu"
         SceneManager.LoadScene("Scene01");
     }
+    
+    public void QuitGame()
+    {
+     // save any game data here
+     #if UNITY_EDITOR
+         // Application.Quit() does not work in the editor so
+         // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+         UnityEditor.EditorApplication.isPlaying = false;
+     #else
+         Application.Quit();
+     #endif
+     
+     }
 }
